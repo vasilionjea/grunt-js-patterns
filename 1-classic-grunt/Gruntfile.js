63750399e071bdd26a9920c59c281dfb24546b90
Billy Onjea <vasili.onjea@gmail.com>
2014-01-05 17:38:27 -0500

    configured 'concat' task for the CSS assets

--- 1-classic-grunt/Gruntfile.js
+++ 1-classic-grunt/Gruntfile.js
@@ -2,8 +2,23 @@ module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-    
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {},
+
+      css: {
+        src: [
+          'css/normalize.css',
+          'css/dropdown.css',
+          'css/tables.css',
+          'css/icons.css',
+          'css/style.css'
+        ],
+
+        dest: 'dist/css/style.css'
+      }
+    }
   });
 
 
@@ -11,4 +26,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+  // Register tasks
+  grunt.registerTask('default', ['concat']);
 };
