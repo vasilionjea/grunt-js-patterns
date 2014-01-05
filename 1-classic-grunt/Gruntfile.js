5d633b142e1d0d16c93a151d83f4c188685dca31
Billy Onjea <vasili.onjea@gmail.com>
2014-01-05 17:41:13 -0500

    added JS files to the concat task

--- 1-classic-grunt/Gruntfile.js
+++ 1-classic-grunt/Gruntfile.js
@@ -17,6 +17,38 @@ module.exports = function(grunt) {
         ],
 
         dest: 'dist/css/style.css'
+      },
+
+      js_body_vendors: {
+        files: {
+          'dist/js/vendors.js': [
+            'js/vendors/jquery.js',
+            'js/vendors/jquery-migrate.js',
+            'js/vendors/underscore.js',
+            'js/vendors/backbone.js',
+            'js/vendors/jstz.js'
+          ]
+        }
+      },
+
+      js_body_plugins: {
+        files: {
+          'dist/js/plugins.js': [
+            'js/plugins/easing.js',
+            'js/plugins/bootstrap.js',
+            'js/plugins/hotkeys.js',
+            'js/plugins/jqdirect.js'
+          ]
+        }
+      },
+
+      js_body_app: {
+        files: {
+          'dist/js/app.js': [
+            'js/app/app.js',
+            'js/app/app-util.js'
+          ]
+        }
       }
     }
   });
